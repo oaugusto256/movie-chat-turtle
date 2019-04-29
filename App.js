dbe0337d346f1d1abd9d6d21bdc78eaa518b7886
Otavio Augusto <tavioalves@gmail.com>
2019-04-29 20:58:33 -0300

    chore: Install react-navigation lib and insert movies.json data, start to structure the project

--- App.js
+++ App.js
@@ -4,18 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
