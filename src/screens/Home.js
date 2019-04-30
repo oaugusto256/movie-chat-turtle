0bf1651293fc4ebf3ac9fbd19cced268773b7e03
Otavio Augusto <tavioalves@gmail.com>
2019-04-29 21:33:18 -0300

    feat: Start to use the movie data provided and some sample style to be used at the Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import data from '../data/movies.json';
 
 class Home extends Component {
   static navigationOptions = {
@@ -13,15 +14,69 @@ class Home extends Component {
     }
   };
 
+  state = {
+    loading: true,
+    movies: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      loading: false,
+      movies: data.slice(0, 20)
+    });
+  }
+
   render() {
+    const { movies } = this.state;
+
     return (
       <>
-        <View>
+        <View style={styles.screen}>
           <Text>Home screen</Text>
+          {movies.map((movie, index) => {
+            return (
+              <View key={index}>
+                <Text>{movie.title}</Text>
+              </View>
+            );
+          })}
         </View>
       </>
     );
   }
 }
+
+const { height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    height: height
+  },
+  loadingContainer: {
+    flex: 1,
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 20
+  },
+  introText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  emphasizeText: {
+    fontWeight: 'bold'
+  },
+  content: {
+    flex: 1,
+    padding: 15
+  }
+});
 
 export default Home;
