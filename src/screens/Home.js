03778eec5e5338ccbf299638c6391b6dd90904d5
Otavio Augusto <tavioalves@gmail.com>
2019-04-29 23:29:03 -0300

    styles: Add icon at the movie content and style it a little bit more

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import Loading from '../components/Loading';
 import data from '../data/movies.json';
 
@@ -38,11 +39,16 @@ class Home extends Component {
           ) : (
             <ScrollView>
               <View style={styles.content}>
+                <Text style={styles.text}>
+                  Select a <Text style={styles.emphasizedText}>Movie</Text> to
+                  see comments.
+                </Text>
                 <>
                   {movies.map((movie, index) => {
                     return (
                       <View key={index} style={styles.movieContent}>
                         <Text>{movie.title}</Text>
+                        <Ionicons name="ios-arrow-forward" size={20} />
                       </View>
                     );
                   })}
@@ -61,23 +67,41 @@ const { height } = Dimensions.get('window');
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    height: height
+    height: height,
+    backgroundColor: '#d8d8d8'
   },
   text: {
     fontSize: 16,
     marginTop: 10,
-    marginBottom: 20
+    marginBottom: 10
+  },
+  emphasizedText: {
+    fontWeight: 'bold'
   },
   movieContent: {
     flex: 1,
-    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 65,
+    padding: 10,
+    marginBottom: 10,
+    borderWidth: 0,
     borderRadius: 4,
-    borderWidth: 0.5,
-    borderColor: '#d6d7da',
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.0,
+
+    elevation: 1
   },
   content: {
     flex: 1,
-    padding: 15
+    padding: 10
   }
 });
 
